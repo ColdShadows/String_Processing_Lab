09d867e1c04200614239b535c101811922e60785
Travis K <traviskean04@live.com>
2015-07-07 16:22:37 -0400

    String Processing Lab
    modify command line arguments

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Processing_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = args.Length;
+
+            //Reverse order of arguments
+            for( int x = i; x > 0; x--)
+            {
+                Console.WriteLine(args[x -1]);
+
+
+            }
+            Console.WriteLine();
+
+            //Arguments in all upper case
+
+            for(int x = 0; x < i; x++)
+            {
+
+                Console.WriteLine(args[x].ToUpper());
+
+            }
+            Console.WriteLine();
+
+            //Concatenate all arguments into one string, with a comma
+
+            string newString = "";
+            foreach( string s in args)
+            {
+                if (newString == "")
+                    newString = newString + s;
+                else
+                newString = newString + "," + s;
+            
+            }
+            Console.WriteLine(newString);
+
+            Console.ReadLine();
+        }
+    }
+}
